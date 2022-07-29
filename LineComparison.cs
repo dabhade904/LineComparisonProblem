41fafb0d732b60d49624a38b170189df6bdba1de
dabhade904 <dabhade904@gmail.com>
2022-07-29 23:52:26 +0530

    Added : calculate line based on a point consisting of (x,y) co-ordinates

---
+++ LineComparison.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    public class LineComparison
+    {
+        public void lineComLineComparisonProblem()
+        {
+            float distance;
+            Console.WriteLine("enter point x1,y1");
+            float x1=float.Parse(Console.ReadLine());
+            float y1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter point x2,y2");
+            float x2 = float.Parse(Console.ReadLine());
+            float y2 = float.Parse(Console.ReadLine());
+            float res = ((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            distance = (float)Math.Sqrt(res);
+
+            Console.WriteLine("distance is : "+distance);
+
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the line comparison problem");
+            LineComparison obj = new LineComparison();
+            obj.lineComLineComparisonProblem();
         }
     }
 }
